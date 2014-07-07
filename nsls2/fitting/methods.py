117e8ca5b847626660f45f6f6cb5198605e3965a
Thomas A Caswell <tcaswell@bnl.gov>
2014-07-07 08:18:11 -0400

    MNT : moved fit_quad_to_peak to fitting.methods

--- nsls2/fitting/methods.py
+++ nsls2/fitting/methods.py
@@ -13,7 +13,7 @@ from six.moves import zip
 import numpy as np
 from scipy.optimize import leastsq
 
-# ==============================================================================
+# =============================================================================
 # The code is this block is derived from code with the following license
 # (BSD to Jonathan J. Helmus, no year and no organization)
 #
@@ -234,4 +234,47 @@ def _internal2external_grad(xi, bounds):
 
     return ge
 
-# ==============================================================================
+# =============================================================================
+
+
+def fit_quad_to_peak(x, y):
+    """
+    Fits a quadratic to the data points handed in
+    to the from y = b[0](x-b[1])**2 + b[2] and R2
+    (measure of goodness of fit)
+
+    Parameters
+    ----------
+    x : ndarray
+        locations
+    y : ndarray
+        values
+
+    Returns
+    -------
+    b : tuple
+       coefficients of form y = b[0](x-b[1])**2 + b[2]
+
+    R2 : float
+      R2 value
+
+    """
+
+    lenx = len(x)
+
+    # some sanity checks
+    if lenx < 3:
+        raise Exception('insufficient points handed in ')
+    # set up fitting array
+    X = np.vstack((x ** 2, x, np.ones(lenx))).T
+    # use linear least squares fitting
+    beta, _, _, _ = np.linalg.lstsq(X, y)
+
+    SSerr = np.sum(np.power(np.polyval(beta, x) - y, 2))
+    SStot = np.sum(np.power(y - np.mean(y), 2))
+    # re-map the returned value to match the form we want
+    ret_beta = (beta[0],
+                -beta[1] / (2 * beta[0]),
+                beta[2] - beta[0] * (beta[1] / (2 * beta[0])) ** 2)
+
+    return ret_beta, 1 - SSerr / SStot
